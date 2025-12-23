a7d9c12d8fe718c7e49e0e59353901f55a57bd30
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-23 10:32:06 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+#include<math.h> 
+int main() { 
+int w; 
+printf("enter the value"); 
+scanf("%d",&w); 
+//for true its 1 and for false its 0 as output 
+printf("%d",(w % 2 == 0)); 
+return 0; 
+} 
