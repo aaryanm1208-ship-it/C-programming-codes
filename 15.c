286add20adc37f9fcd5231add3edf1a1fe961078
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-23 10:34:33 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,32 @@
+#include <stdio.h> 
+int main() { 
+int age; 
+printf("enter the value of your age"); 
+scanf("%d",&age); 
+if(age > 18){ 
+printf("i am an adult\n"); 
+printf("i am eligible to vote\n"); 
+printf("i can have driving license\n"); 
+} 
+else{ 
+printf("not an adult"); 
+} 
+printf("thank you"); 
+return 0; 
+} 
+#include <stdio.h> 
+int main() { 
+int age; 
+printf("enter the value of your age"); 
+scanf("%d",&age); 
+if(age > 18){ 
+printf("i am an adult\n"); 
+printf("i am eligible to vote\n"); 
+printf("i can have driving license\n"); 
+} 
+else{ 
+printf("not an adult"); 
+} 
+printf("thank you"); 
+return 0; 
+} 
