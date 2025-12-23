d78177a0681929c4c420f70e93559bb67efe5105
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-23 10:32:26 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,12 @@
+//to see weather the number is even o odd  
+/* if the value is 1 so its even and if its 0 then its 0 */ 
+#include<stdio.h> 
+#include<math.h> 
+int main() 
+{ 
+int a; 
+printf("enter the value"); 
+scanf("%d",&a); 
+printf("%d", a % 2 ==0); 
+return 0; 
+}
