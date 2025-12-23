dbe24495c8c9e8ece40481a3f7a97fcf36bd093a
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-23 10:26:14 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include<math.h> 
+int main() { 
+int a=2; 
+int b=a; 
+int c= 7-a , e; 
+printf("enter the value"); 
+scanf("%d",&e); 
+printf("%d\n%d\n%d\n",a,b,c); 
+return 0; 
+} 
