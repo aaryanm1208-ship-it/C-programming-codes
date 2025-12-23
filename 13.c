93c539bcc77722dc88061f0957acefd904447892
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-23 10:33:31 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,10 @@
+// to check weather the number is 2 digit 
+#include <stdio.h> 
+#include<math.h> 
+int main() { 
+int a; 
+printf("enter the value of a"); 
+scanf("%d",&a); 
+printf("%d",(a>9) && (a<100)); 
+return 0; 
+}
