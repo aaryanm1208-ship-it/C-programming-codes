a0290f6218104f76bfd30e4c35efac68d62a6d65
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-23 10:32:52 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,17 @@
+// print if its monday or its raining ==> true 
+#include <stdio.h> 
+#include<math.h> 
+int main() { 
+int a=1; 
+int b=0; 
+printf("%d%d",a&&b); 
+return 0; 
+}// print if its monday or its raining ==> true 
+#include <stdio.h> 
+#include<math.h> 
+int main() { 
+int a=1; 
+int b=0; 
+printf("%d%d",a&&b); 
+return 0; 
+}
