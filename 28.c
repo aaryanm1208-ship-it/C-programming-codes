edaacbde6223e90253066a3e1a4ce83391cbe778
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-24 09:41:44 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,14 @@
+//Using do while loop 
+#include<stdio.h> 
+int main() 
+{ 
+int i; 
+printf("enter the value"); 
+scanf("%d",&i); 
+do{ 
+printf("%d",&i); 
+i++; 
+} 
+while(i<=5); 
+return 0; 
+}
