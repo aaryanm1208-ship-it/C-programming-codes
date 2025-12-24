27618f0786e05baf1af0399773eee4d24d1b59b0
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-24 09:36:26 +0530

    Create 18.c

---
+++ 18.c
@@ -0,0 +1,31 @@
+// Hum Aaj Karenge Switch Case Checking DAYS 1 TO 7 
+#include<stdio.h> 
+int main() 
+{ 
+int day;//1-M,2-T,3-W,4-TH,5-F,6-S,7-SUN 
+printf("KAUNSA BHI DIVAS ENTER KARO 1to7\n"); 
+scanf("%d",&day); 
+switch(day){ 
+case 1 : printf("Monday\n"); 
+break; 
+case 2 : printf("Tuesday\n"); 
+break; 
+case 3 : printf("Wednesday\n"); 
+break; 
+case 4 : printf("Thursday\n"); 
+break; 
+case 5 : printf("Friday\n"); 
+break; 
+case 6 : printf("Saturday\n"); 
+break; 
+case 7 : printf("Sunday\n"); 
+break; 
+default : printf("kuch toh galti ki hai sahab 
+aapne\n"); 
+break; 
+} 
+printf("SHUKRIYA AAPKA , AAPNE ISKELIYE APNA 
+MAULYAVAAN SAMAY GAVAYA HAI ISILIYE MEIN 
+AAPKA AABHARI HOO\n"); 
+return 0; 
+}
