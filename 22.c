fc10fab329c980b0662b10671eea73750755131a
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-24 09:38:50 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,12 @@
+//Using for loop  
+#include<stdio.h> 
+int main() 
+{ 
+int i; 
+printf("ENTER VALUE OF i\n"); 
+scanf("%d",&i); 
+for(i ; i<=18 ; i=i+1){ 
+printf("Hello Aaryan\n"); 
+} 
+return 0; 
+}
