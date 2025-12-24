4149cf027a3ff7f505a4776c06d1d26970a5f3fb
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-24 09:35:47 +0530

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,10 @@
+//use of ternery operator 
+#include <stdio.h> 
+int main() { 
+int age; 
+printf("ENTER YOUR AGE"); 
+scanf("%d",&age); 
+age>=18? printf("ADULT HO AAP") : printf("ADULT 
+NAHI HO AAP"); 
+return 0; 
+} 
